b0b11ef9e6da8e075553338a7af96e55ab778e80
juaji <supahuman>
2024-04-23 10:37:24 -0500

    Set up Category Model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //properties
+
+        [Key]//primary key
+        public int Id { get; set; }
+        [Required]//No null values allowed
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
